ee28a841b8f297bf7516db6702557e137ca00b3e
Masooma Farhang <masoomafarhang@gmail.com>
2023-05-18 14:01:41 -0400

    Initial commits (Week1 and .gitignore file)

---
+++ src/week01/Odd_Even.java
@@ -0,0 +1,15 @@
+package week01;
+
+public class Odd_Even {
+    /*
+Numbers-Odd or even:
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+ */
+    public static void main(String[] args) {
+
+    }
+
+}
