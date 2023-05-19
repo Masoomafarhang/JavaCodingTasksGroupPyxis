3e79b8f9e6460f12000d781eb85983eeeed18243
flyhigh2 <flyhighoym2@yahoo.ca>
2023-05-18 21:30:10 -0400

    coding done

--- src/week01/Odd_Even.java
+++ src/week01/Odd_Even.java
@@ -1,15 +1,24 @@
 package week01;
 
-public class Odd_Even {
-    /*
+import java.util.Scanner;
+
+/*
 Numbers-Odd or even:
 Write  a method which can identify given number is even or odd
 Output ex:
 identify(5); ->"Odd"
 identify(6); ->"Even"
- */
+*/
+public class Odd_Even {
+
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number for check odd or even ");
+        int num = scan.nextInt();
+
+
     }
+
 
 }
