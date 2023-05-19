5923752d956b1af5484950305a186e10eac17336
flyhigh2 <flyhighoym2@yahoo.ca>
2023-05-18 23:14:38 -0400

    finra and odd&even

--- src/week01/Odd_Even.java
+++ src/week01/Odd_Even.java
@@ -1,5 +1,7 @@
 package week01;
 
+import java.util.Scanner;
+
 public class Odd_Even {
     /*
 Numbers-Odd or even:
@@ -10,6 +12,15 @@ identify(6); ->"Even"
  */
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 
 }
