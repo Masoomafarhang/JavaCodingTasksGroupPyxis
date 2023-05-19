4d5c6d32baa38fd3dd0df4664a49cc1664d43f87
flyhigh2 <flyhighoym2@yahoo.ca>
2023-05-18 23:32:06 -0400

    odd and even   coding done

--- src/week01/Odd_Even.java
+++ src/week01/Odd_Even.java
@@ -11,16 +11,13 @@ identify(5); ->"Odd"
 identify(6); ->"Even"
  */
     public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int num = input.nextInt();
-
-        if (num % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
-        }
+     EvenOrOdd(6);
     }
-
+   public static void EvenOrOdd(int num ) {
+       if (num % 2 == 0) {
+           System.out.println(num + " is Even");
+       } else {
+           System.out.println(num + " is Odd");
+       }
+   }
 }
