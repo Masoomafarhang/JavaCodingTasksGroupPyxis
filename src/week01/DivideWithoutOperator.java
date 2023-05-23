a2d48aec110d5c3c192b9c5320750a0cb2cd4ad6
flyhigh2 <flyhighoym2@yahoo.ca>
2023-05-23 13:39:29 -0400

    DivideWithoutOperator is compeleted

--- src/week01/DivideWithoutOperator.java
+++ src/week01/DivideWithoutOperator.java
@@ -1,12 +1,31 @@
 package week01;
 
-public class DivideWithoutOperator {
+public abstract class DivideWithoutOperator {
     /*
     Numbers - Divide without / operator:
 Write a method that can divide two numbers without using division operator
      */
     public static void main(String[] args) {
+        System.out.println(Calculator(100, 10));
 
     }
 
+
+
+    public static int Calculator (int a , int b){
+        int count =0;
+        System.out.print(a + " divided by " + b +" is " );
+        while(a>=b){
+            a-=b;
+            count++;
+
+        }
+
+        return count;
+    }
+
+     
+
+
 }
+
