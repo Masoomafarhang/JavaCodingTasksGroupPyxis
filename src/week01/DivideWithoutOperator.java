4098871c5f9af3db7c50f5d566122236442ae329
Nadi <nadi9103@yahoo.com>
2023-05-25 10:49:04 -0400

    added solution for the task .

--- src/week01/DivideWithoutOperator.java
+++ src/week01/DivideWithoutOperator.java
@@ -1,12 +1,29 @@
 package week01;
 
 public class DivideWithoutOperator {
+
+
+
+
+        public static void main(String[] args) {
+            int a = 50;
+            int b = 9;
+
+            int count = 0;
+
+            while (a >= b) {
+                a -= b;
+                count++;
+            }
+
+            System.out.println(count+" with a remainder of "+a);
+
+    }
+
+
+
+}
     /*
     Numbers - Divide without / operator:
 Write a method that can divide two numbers without using division operator
      */
-    public static void main(String[] args) {
-
-    }
-
-}
