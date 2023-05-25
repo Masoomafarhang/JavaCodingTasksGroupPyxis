21c776619384226e6979c7dc83c05658ea095203
StarMustafa <musmusta1999@gmail.com>
2023-05-24 22:26:02 -0400

    Finished Divide without operator task

--- src/week01/DivideWithoutOperator.java
+++ src/week01/DivideWithoutOperator.java
@@ -5,12 +5,35 @@ public class DivideWithoutOperator {
     Numbers - Divide without / operator:
 Write a method that can divide two numbers without using division operator
      */
+/*
+    Numbers -- Divide without / operator
+            => Write a method that can divide two numbers without using division operator.
+     */
+
+    public static void divideWithoutOperator(double a,double b){
+
+        int count=0;
+        while (a>=b) {
+            a-=b;// >> a = (a-b)>>
+            count++;//+1
+        }
+        System.out.println(count+" reminder "+a);
+    }
+
+    //10 / 2= 5
+    // 10 % 2 == 0
+    // reminder = leftover
     public static void main(String[] args) {
+        divideWithoutOperator(14,3);
+        //       20  | 6,666666
+        //       20
 
-        //my first change
+        int c =11;
+        int d= 5;
 
-        //new changes
-        // more changes
+
+
+
 
     }
 
