54aa926b38eb255d4dee3e8ca38229c911662835
Nadi <nadi9103@yahoo.com>
2023-05-25 11:01:31 -0400

    new changes added task solution

--- src/week01/FINRA.java
+++ src/week01/FINRA.java
@@ -7,7 +7,7 @@ public class FINRA {
         String result= "";
         for (int i = 1; i <= 30; i++) {
 
-            if (i % 15 == 0) {
+            if (i % 3== 0 && i %5==0) {
                 result += " FINRA ";
             } else if (i % 5 == 0) {
                 result += "RA";
