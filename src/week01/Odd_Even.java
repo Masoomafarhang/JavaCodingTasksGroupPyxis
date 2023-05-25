f2a0869dc61eacec5bdfac1d359509fd407548d1
StarMustafa <musmusta1999@gmail.com>
2023-05-24 22:26:51 -0400

    Finished Odd or even task

--- src/week01/Odd_Even.java
+++ src/week01/Odd_Even.java
@@ -1,6 +1,8 @@
 package week01;
 
 public class Odd_Even {
+
+
     /*
 Numbers-Odd or even:
 Write  a method which can identify given number is even or odd
@@ -9,6 +11,12 @@ identify(5); ->"Odd"
 identify(6); ->"Even"
  */
     public static void main(String[] args) {
+        System.out.println(OddOrEven(3));
+    }
+
+    public static String OddOrEven(int num) {
+
+        return (num % 2==0)?"Even":"Odd";
 
     }
 
