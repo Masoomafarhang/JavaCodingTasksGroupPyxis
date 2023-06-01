c3e353979eaf9cf15a2b2840026d7afa8ad211be
14372 <o.teyfiktavli@gmail.com>
2023-06-01 16:19:53 -0400

    uptaded the error

--- src/week01/Odd_Even.java
+++ src/week01/Odd_Even.java
@@ -11,17 +11,18 @@ identify(6); ->"Even"
     public static void main(String[] args) {
 
 
-        int [] num={2,1,3,4,5,6,7,8,9,10,12};
+        int[] num = {2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 12};
 
         for (int each : num) {
-            if(each%2==0){
-                System.out.println("Even numbers: "+each);
+            if (each % 2 == 0) {
+                System.out.println("Even numbers: " + each);
             }
-            if(each%2!=0){
-                System.out.println("Odd numbers: "+each);
+            if (each % 2 != 0) {
+                System.out.println("Odd numbers: " + each);
             }
 
+
+        }
 
     }
-
 }
