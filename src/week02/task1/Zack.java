a3d03ab5d4245f405c7305520182c331e8b3ecd9
Ahtemn <124060175+Ahtemn@users.noreply.github.com>
2023-06-01 12:26:20 -0400

    Completed week 2 tasks

--- src/week02/task1/Zack.java
+++ src/week02/task1/Zack.java
@@ -6,6 +6,20 @@ public class Zack {
     Swap two variable values without using a third variable
     */
     public static void main(String[] args) {
+    swapTwoVariable(11,33);
+
+
+    }
+    public static void swapTwoVariable (int a, int b){
+        System.out.println("You have entered \nint a= "+a + "\n"+ "int b= "+b);
+        System.out.println("After the swapping new variables are ");
+        a = a+b;//25
+
+        b=a-b;//25-15=10
+
+        a=a-b; // a = 25-10=15
+        System.out.println("int a= "+a+"\n"+ "int b= "+ b);
+
 
     }
 }
