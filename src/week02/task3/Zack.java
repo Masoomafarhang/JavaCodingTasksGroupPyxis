72436e56e74d037281207ba3c296945144b2ff4c
flyhigh2 <flyhighoym2@yahoo.ca>
2023-06-01 11:55:03 -0400

    DivideWithoutOperator

--- src/week02/task3/Zack.java
+++ src/week02/task3/Zack.java
@@ -7,10 +7,31 @@ public class Zack {
     public static void main(String[] args) {
 
 
+        String result = "";
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 30 == 0)
+                result += "CodilityTestCoders ";
+            else if (i % 5 == 0)
+                result += "Coders ";
+            else if (i % 3 == 0)
+                result += "Test ";
+            else if (i % 2 == 0)
+                result += "Codility ";
+            else if (i % 6 == 0)
+                result += "CodilityTest ";
+            else if (i % 10 == 0)
+                result += "CodilityCoders ";
+            else if (i % 15 == 0)
+                result += "TestCoders ";
+            else
+                result += i + " ";
         }
-
+        System.out.println(result);
     }
+}
+
+
 /*
     ===== Numbers -- print consecutive numbers ===============
     Write a function that, given a positive integer N, prints the consecutive
