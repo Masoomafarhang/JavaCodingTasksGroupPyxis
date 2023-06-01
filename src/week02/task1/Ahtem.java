cd300e4d0ff4fe86094643ea2ca0ab39ce6af0fc
Ahtemn <124060175+Ahtemn@users.noreply.github.com>
2023-06-01 14:41:52 -0400

    i missed some part when i do copy paste. so I added them

--- src/week02/task1/Ahtem.java
+++ src/week02/task1/Ahtem.java
@@ -1,6 +1,9 @@
 package week02.task1;
 
 public class Ahtem {
+    public static void main(String[] args) {
+        swapTwoVariable(10,13);
+    }
     public static void swapTwoVariable (int a, int b){
         System.out.println("You have entered \nint a= "+a + "\n"+ "int b= "+b);
         System.out.println("After the swapping new variables are ");
