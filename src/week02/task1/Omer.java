88f60af83b223c32041c3912e51afb848f6e35ad
14372 <o.teyfiktavli@gmail.com>
2023-06-01 21:44:03 -0400

    solved

--- src/week02/task1/Omer.java
+++ src/week02/task1/Omer.java
@@ -1,4 +1,41 @@
 package week02.task1;
 
 public class Omer {
+    public static void main(String[] args) {
+
+        int a =40;
+        int b=390;
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        int c=10;
+        int d=30;
+        int f;
+
+        f=c;
+        c=d;
+        d=f;
+        f=0;
+
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+
+
+
+
+
+    }
 }
+/*   /*
+    ========== Numbers -- Swap Numbers =========
+    Swap two variable values without using a third variable
+    */
