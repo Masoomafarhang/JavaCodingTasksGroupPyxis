3d5b4889475987d237cd6f25dadb5b2cf8ef16c9
Ahtemn <124060175+Ahtemn@users.noreply.github.com>
2023-06-06 23:40:38 -0400

    Completed Prime number Task

---
+++ src/week03/PrimeNumber.java
@@ -0,0 +1,32 @@
+package week03;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+     /*1️⃣ Numbers -- PrimeNumber
+        Write a method that can check if a number is prime or not */
+    primeNumber(13);
+    }
+        public static void primeNumber(int a){
+        int count =0;
+        for (int i = 1; i < a; i++) {
+            if((a % i == 0 )){
+                count++;
+            }
+            }if (count==1){
+                System.out.println(a + " is a PrimeNumber");
+            }else {
+                System.out.println(a + " is not a prime number");
+            }
+        }
+    }
+
+
+
+
+
+/*2️⃣ Numbers -- Reverse Negative Number
+        Write a return method that can reverse digits of a negative number and return it as int
+        input: -35  output: -53
+
+      */
