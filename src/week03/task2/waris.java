d8d3e7aed881ba9f70eca44bdb783c381d62b4aa
flyhigh2 <flyhighoym2@yahoo.ca>
2023-06-08 00:14:21 -0400

    DivideWithoutOperator

---
+++ src/week03/task2/waris.java
@@ -0,0 +1,19 @@
+package week03.task2;
+
+public class waris {
+    public static void main(String[] args) {
+        reverse(-2865);
+    }
+    public static void reverse(int number) {
+        if (number < 0) {
+            System.out.print("-");
+            reverse(number * -1);
+        } else if (number < 10) {
+            System.out.println(number);
+        } else {
+            System.out.print(number % 10);
+            reverse(number / 10);
+        }
+
+    }
+}
