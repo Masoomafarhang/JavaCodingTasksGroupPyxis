da892d695fabce588022a0ab66969f4d8b78c7e2
StarMustafa <musmusta1999@gmail.com>
2023-06-16 14:54:50 -0400

    Finished Tasks 1,2,3 from week4

---
+++ src/week03/Task1/Mustafa.java
@@ -0,0 +1,34 @@
+package week03.Task1;
+
+import java.sql.PreparedStatement;
+
+public class Mustafa {
+   /* Numbers -- PrimeNumber
+    Write a method that can check if a number is prime or not */
+
+    public static void main(String[] args) {
+        int num = 25;
+
+        if (primNumber(num)) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+
+    }
+
+    public static boolean primNumber(int num) {
+
+
+        for (int i = 2; i < num; i++) {
+
+            if (num % i == 0) {
+
+
+                return false;
+            }
+        }
+
+        return true ;
+    }
+}
