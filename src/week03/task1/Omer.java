3461c831248521ce0a9c7b88ceec65014f504544
14372 <o.teyfiktavli@gmail.com>
2023-06-26 15:37:35 -0400

    solved

---
+++ src/week03/task1/Omer.java
@@ -0,0 +1,18 @@
+package week03.task1;
+
+public class Omer {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+/*
+
+
+Numbers -- PrimeNumber
+Write a method that can check if a number is prime or not
+
+*/
