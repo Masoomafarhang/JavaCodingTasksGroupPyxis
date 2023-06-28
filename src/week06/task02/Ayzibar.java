ffe7cdaf3b90ae59ab2d1cff04a264dfbb9b597b
ayzibarak <ayzibarak@gmail.com>
2023-06-28 22:06:28 +0300

    Task02-Ayzibar

---
+++ src/week06/task02/Ayzibar.java
@@ -0,0 +1,23 @@
+package week06.task02;
+
+public class Ayzibar {
+
+    static int arr[] = {99, 12, 23, 32, 44, 57, 6};
+
+    static int largest() {
+
+        int i;
+        int max = arr[0];
+
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Largest in given array is " + largest());
+    }
+
+}
