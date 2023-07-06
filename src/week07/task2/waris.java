ef2087dff03420b02bd88e7c38d14fb142231cdd
flyhigh2 <flyhighoym2@yahoo.ca>
2023-07-05 23:58:36 -0400

    DivideWithoutOperator

---
+++ src/week07/task2/waris.java
@@ -0,0 +1,13 @@
+package week07.task2;
+
+import java.util.Arrays;
+
+public class waris {
+    //Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+    //Ex: int[] arr = {10, 9, 8, 7};
+    //arr = sortAscending(arr); ==>{ 7, 8, 9, 10};
+
+
+    }
+
+
